831f56466c7ccb1e833c1edb1684fa7095e5911c
Zxy <1986319485@qq.com>
2022-09-17 10:57:20 +0800

    second commit

---
+++ src/main/java/com/zxy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zxy.git;
+
+/**
+ * @ClassName GitTest
+ * @Author zxy
+ * @Date 2022/9/17 10:54
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
