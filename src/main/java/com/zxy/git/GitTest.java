3e2d405a405f5efbfdf4d5a6a29ddc17cc679ddb
zhouxygithub <113569933+zhouxygithub@users.noreply.github.com>
2022-09-17 16:22:03 +0800

    pull commit

--- src/main/java/com/zxy/git/GitTest.java
+++ src/main/java/com/zxy/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("master test1");
+        System.out.println("pull test");
     }
 }
